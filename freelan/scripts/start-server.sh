d137276f7afde7bd323edefc0c5d0c3e13d785f6
jhearn85 <hearnjameson@gmail.com>
2021-04-05 21:26:24 -0700

    removed unused apps

--- freelan/scripts/start-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
-MODE=debug
-
-${SCRIPTPATH}/../build/${MODE}/bin/freelan -f -d --server.enabled=yes --server.authentication_script=${SCRIPTPATH}/authenticate.sh $@
